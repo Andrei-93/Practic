7e66c6441529af161f7f8cd7d858f8811962c69a
andrei <agabalaev_1993@mail.ru>
2020-12-14 16:10:51 +0200

    my project

---
+++ practic.js
@@ -0,0 +1,20 @@
+
+ const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     acturs: {},
+     genres: [],
+     privat: false
+ };
+
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('Насколько вы его оцениваете?', ''),
+       c = prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('Насколько вы его оцениваете?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
