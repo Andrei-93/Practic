f3401c45c51922c9744a9458244cb360c296e3da
andrei <agabalaev_1993@mail.ru>
2020-12-17 16:51:15 +0200

    practic2

--- practic.js
+++ practic.js
@@ -1,20 +1,35 @@
+const numberOfFilms = +prompt('сколько фильмов вы уже посмотрели?', '');
 
- const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    acturs : {},
+    genres : [],
+    privat : false
+};
 
- const personalMovieDB = {
-     count: numberOfFilms,
-     movies: {},
-     acturs: {},
-     genres: [],
-     privat: false
- };
 
- const a = prompt('Один из последних просмотренных фильмов?', ''),
-       b = prompt('Насколько вы его оцениваете?', ''),
-       c = prompt('Один из последних просмотренных фильмов?', ''),
-       d = prompt('Насколько вы его оцениваете?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+          b = prompt('На сколько он вам понравился?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b; 
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель!');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман!');
+} else {
+    console.log('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
